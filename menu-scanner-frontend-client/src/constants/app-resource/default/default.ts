7afafdccf0c5795ef3807751bebac29ab0782545
Phat Menghor <phatmenghor19@gmail.com>
2026-03-20 23:21:49 +0700

    [claude/fix-payment-api-endpoints-EMUWw] Auto commit on 2026-03-20 23:21:49 | Files: menu-scanner-frontend-client/src/constants/app-resource/default/default.ts

--- menu-scanner-frontend-client/src/constants/app-resource/default/default.ts
+++ menu-scanner-frontend-client/src/constants/app-resource/default/default.ts
@@ -1,7 +1,7 @@
 export const AppDefault = {
   RESET_PASSWORD: "88889999",
   // BUSINESS_ID: "826dc79c-8ec4-4bfe-8d24-f6fd28d18487",
-  BUSINESS_ID: "25fc7784-4f32-44dd-8471-a1dd5b040f79",
+  BUSINESS_ID: "dfd683aa-03ee-42c3-b223-66c085e6c36b",
   PAGE_SIZE: 15,
   PAGE_SIZE_OPTIONS: [10, 15, 20, 50, 100],
 };
