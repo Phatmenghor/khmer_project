2246235505808e03f2ec670a9b2a322ba7915774
Phat Menghor <menghor.phat@cambodiapostbank.com.kh>
2026-03-24 10:02:30 +0700

    [claude/checkout-page-delivery-payment-NO1MS] Auto commit on 2026-03-24 10:02:30 | Files: menu-scanner-frontend-client/src/constants/app-resource/default/default.ts

--- menu-scanner-frontend-client/src/constants/app-resource/default/default.ts
+++ menu-scanner-frontend-client/src/constants/app-resource/default/default.ts
@@ -1,7 +1,7 @@
 export const AppDefault = {
   RESET_PASSWORD: "88889999",
   // BUSINESS_ID: "826dc79c-8ec4-4bfe-8d24-f6fd28d18487",
-  BUSINESS_ID: "435d4ba6-945f-40db-bb1d-91908d51903d",
+  BUSINESS_ID: "8b5e177e-3458-48e8-b963-c82efddee58c",
   PAGE_SIZE: 15,
   PAGE_SIZE_OPTIONS: [10, 15, 20, 50, 100],
 };
