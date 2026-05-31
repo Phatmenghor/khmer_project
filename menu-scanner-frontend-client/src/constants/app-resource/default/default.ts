3f14e2aab35e49f6f5f6a2f5381715d219215155
Phat Menghor <phatmenghor19@gmail.com>
2026-05-31 12:44:33 +0700

    [claude/happy-bardeen-8nSRf] Auto commit on 2026-05-31 12:44:33 | Files: src/constants/app-resource/default/default.ts

--- menu-scanner-frontend-client/src/constants/app-resource/default/default.ts
+++ menu-scanner-frontend-client/src/constants/app-resource/default/default.ts
@@ -1,14 +1,12 @@
 export const AppDefault = {
   RESET_PASSWORD: "88889999",
 
-  BUSINESS_ID: "550cad56-cafd-4aba-baef-c4dcd53940d0",
+  BUSINESS_ID: "78e375fa-386d-4de2-a485-ee7a9a6c4bdf",
   PAGE_SIZE: 15,
   PAGE_SIZE_OPTIONS: [10, 15, 20, 50, 100],
 };
 
-
 export const SocialAuthConfig = {
-
   TELEGRAM_BOT_NAME:
     process.env.NEXT_PUBLIC_TELEGRAM_BOT_NAME || "CambodiaEMenuBot",
 
