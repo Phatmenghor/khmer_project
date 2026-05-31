7617715b218b1327d04e26611dce9b123b560c45
Phat Menghor <phatmenghor19@gmail.com>
2026-05-31 14:34:35 +0700

    [claude/happy-bardeen-8nSRf] Auto commit on 2026-05-31 14:34:35 | Files: src/constants/app-resource/default/default.ts

--- menu-scanner-frontend-client/src/constants/app-resource/default/default.ts
+++ menu-scanner-frontend-client/src/constants/app-resource/default/default.ts
@@ -1,7 +1,7 @@
 export const AppDefault = {
   RESET_PASSWORD: "88889999",
 
-  BUSINESS_ID: "78e375fa-386d-4de2-a485-ee7a9a6c4bdf",
+  BUSINESS_ID: "550cad56-cafd-4aba-baef-c4dcd53940d0",
   PAGE_SIZE: 15,
   PAGE_SIZE_OPTIONS: [10, 15, 20, 50, 100],
 };
